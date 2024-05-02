cd60f1c18f69525db83539d6fda98e7932a327d3
NarmeenAdeel1 <narminsiddiqui@gmail.com>
2024-05-02 04:16:58 -0700

    ádding a currency_converter

---
+++ index.ts
@@ -0,0 +1,43 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer"
+
+const currency: any = {
+    USD: 1, //BASE CURRENCY
+    EUR: 0.91,
+    GBP: 0.76,
+    INR: 74.57,
+    PKR: 280
+};
+
+
+let user_answer = await inquirer.prompt(
+    [
+    {
+        name: "from",
+        message: "enter from currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    { 
+        name: "to",
+        message: "enter to currency",
+        type: "list",
+        choices: ["USD", "EUR", "GBP", "INR", "PKR"]
+    },
+    {
+        name: 'amount',
+        message: 'enter your amount',
+        type: 'number'
+    }
+
+],
+
+    ) 
+   let fromAmount = currency[user_answer.from]
+    let toAmount = currency[user_answer.to]  
+    let amount = user_answer.amount  
+    let baseamount = amount / fromAmount //USD BASE CURRENCY //4
+    let convertedAmount = baseamount * toAmount
+    console.log(convertedAmount);
+  
